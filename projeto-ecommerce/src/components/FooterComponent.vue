8802edad60791fcd0d35e5e65743bec37fbd650d
caiogsetec <143644880+caiogsetec@users.noreply.github.com>
2025-06-03 16:24:00 -0300

    Delete projeto-ecommerce directory

--- projeto-ecommerce/src/components/FooterComponent.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <footer class="bg-orange text-white p-4 text-center">
-    <p>&copy; 2025 - E-commerce Turma C - ADS</p>
-  </footer>
-</template>
--- projeto-ecommerce/src/views/HomeView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="text-center">
-    <h1 class="text-4xl font-bold mb-4">Bem-vindo à nossa loja</h1>
-    <p class="text-lg">Veja nossos produtos incríveis na aba de produtos!</p>
-  </div>
-</template>
